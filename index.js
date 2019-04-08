078df4267718613af8125bfa0a43fb35a727deb0
Thinh NND <16521179@gm.uit.edu.vn>
2019-04-08 14:43:14 +0700

    add function register

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const users = require('./routes/users')
+const cors = require('cors')
 
 //setup environment
 dotenv.config()
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true })
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
+
 app.use('/api/users', users)
 
 const PORT = process.env.PORT || 5000
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,40 @@
 const router = require('express').Router()
 const User = require('../models/User')
+const validateRegisterInput = require('../validation/register')
+const bcrypt = require('bcryptjs')
 
-router.route('/').get((req, res) => {
-    res.send('Hello')
+router.route('/register').post((req, res) => {
+    const { isValid, errors } = validateRegisterInput(req.body)
+    
+    if(!isValid) {
+        return res.status(404).json(errors)
+    }
+
+    User.findOne({ email: req.body.email } )
+        .then(user => {
+            if (user) {
+                errors.email = 'Email was used'
+                return res.send.status(404).json(errors)
+            }
+
+            bcrypt.genSalt(10, function(err, salt) {
+                bcrypt.hash(req.body.password, salt, function (err, hash) {
+                    const newUser = new User({
+                        email: req.body.email,
+                        login: req.body.login,
+                        password: hash
+                    })
+
+                    newUser.save()
+                        .then(newUser => res.json(newUser))
+                        .catch(err => console.log(err))
+                })
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
 })
 
 module.exports = router
