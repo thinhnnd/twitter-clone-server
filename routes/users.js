10cb60c2e88208b0382d1e80aeb935fa10a8751c
Thinh NND <16521179@gm.uit.edu.vn>
2019-04-09 10:46:56 +0700

    config passport passport-jwt and jsonwebtoken

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const User = require('../models/User')
-const validateRegisterInput = require('../validation/register')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const validateRegisterInput = require('../validation/register')
+const validateLoginInput = require('../validation/login')
 
 router.route('/register').post((req, res) => {
     const { isValid, errors } = validateRegisterInput(req.body)
@@ -14,7 +17,7 @@ router.route('/register').post((req, res) => {
         .then(user => {
             if (user) {
                 errors.email = 'Email was used'
-                return res.send.status(404).json(errors)
+                return res.status(404).json(errors)
             }
 
             bcrypt.genSalt(10, function(err, salt) {
@@ -35,6 +38,38 @@ router.route('/register').post((req, res) => {
             console.log(err)
         })
 
+})
+
+router.route('/login').post((req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body)
+
+    if(!isValid) {
+        return res.status(404).json(errors)
+    }
+    User.findOne({ email: req.body.email } )
+        .then( user => {
+            if(user) {
+                bcrypt.compare(req.body.password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '1d'}, function(err, token) {
+                            return res.json({
+                                success: true,
+                                token: token
+                            })
+                        })
+                    }
+                    else {
+                        errors.password = 'Password is in correct'
+                        return res.status(404).json(errors)
+                    }
+                }) 
+            } else {
+                errors.email = 'User not found'
+                return res.status(404).json(errors)
+            } 
+        })
+        .catch(err=> console.log(err))
 })
 
 module.exports = router
--- validation/login.js
+++ validation/login.js
@@ -11,26 +11,12 @@ module.exports = function (data) {
         errors.email = 'Email is invalid'
     }
 
-    if (Validator.isEmpty(data.login)) {
-        errors.login = 'Login field is required'
-    }
-
-    if (!Validator.isLength(data.login, {min: 4, max: 32})) {
-        errors.login = 'Login must bettween 4 and 32 characters'
-    }
-
     if (Validator.isEmpty(data.password)) {
         errors.password = 'Password field is required'
     }
 
     if (!Validator.isLength(data.password, {min: 6, max: 32})) {
         errors.password = 'Password must bettween 6 and 32 characters'
-    }
-
-    if (Validator.isEmpty(data.password2)) {
-        errors.password2 = 'Retyped password field is required'
-    } else if (!Validator.equals(data.password, data.password2)) {
-        errors.password2 = 'Your retyped password must match'
     }
 
     return {
